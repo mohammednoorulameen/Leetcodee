5f7e771744c08db9b9b55cb3d0e802561c7e38e0
MOHAMMED NOORUL AMEEN <96298671+mohammednoorulameen@users.noreply.github.com>
2026-08-07 11:19:36 +0530

    Time: 7 ms (20.05%), Space: 54.5 MB (97.89%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -6,9 +6,8 @@
 var nextGreaterElement = function (nums1, nums2) {
 
     let arr = [];
-    let index = 0
     for (let i = 0; i < nums1.length; i++) {
-        index = nums2.indexOf(nums1[i])
+        let index = nums2.indexOf(nums1[i])
         let found = -1
         for (let j = index + 1; j < nums2.length; j++) {
             if (nums2[j] > nums1[i]) {
@@ -17,8 +16,6 @@ var nextGreaterElement = function (nums1, nums2) {
             }
         }
         arr.push(found)
-
     }
-    console.log(arr)
     return arr
 };
