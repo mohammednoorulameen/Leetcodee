70dca03338f2ae68832f2d3ad20eada19e7f45ae
MOHAMMED NOORUL AMEEN <96298671+mohammednoorulameen@users.noreply.github.com>
2026-09-12 21:27:56 +0530

    Time: 0 ms (100%), Space: 54.2 MB (45.75%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -5,10 +5,9 @@
 var rotate = function(matrix) {
 
      matrix.reverse();
-     let n = matrix.length
 
-    for(let i = 0; i < n; i++){
-        for(let j = i+1; j < n; j++){
+    for(let i = 0; i < matrix.length; i++){
+        for(let j = i+1; j < matrix.length; j++){
             let temp = matrix[i][j]
             matrix[i][j] = matrix[j][i]
             matrix[j][i] = temp;
