36e4a7c68c6f160e15619366d876544f0f7937b9
MOHAMMED NOORUL AMEEN <96298671+mohammednoorulameen@users.noreply.github.com>
2026-09-15 20:53:18 +0530

    Time: 81 ms (0%), Space: 62 MB (7.69%) - LeetHub

---
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @param {character} fill
+ * @return {string[]}
+ */
+var divideString = function(s, k, fill) {
+    
+    let arr = [];
+    let str = ""
+    let count = 0;
+    for(let i = 0; i < s.length; i++){
+        str += s[i];
+        console.log(str)
+        count++;
+        if(count == k){
+            console.log('check this working ')
+            arr.push(str)
+            str = ""
+            count = 0;
+        }
+    }
+
+    if(str.length > 0){
+        while(str.length < k){
+         str += fill;
+    }
+    arr.push(str)
+    }
+
+return arr
+ console.log(arr)
+
+};
